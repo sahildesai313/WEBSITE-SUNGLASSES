5c305e6613a980b3a59a08ea59ab2c0906cd7b7a
sahil <sahilpatel313200@gmail.com>
2024-02-05 12:59:24 +0530

    login page

--- lr/glasses/admin.py
+++ lr/glasses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import person
 
-# Register your models here.
+class personAdmin(admin.ModelAdmin):
+    list_display = ("username","fullname","email","phone","gender",)
+admin.site.register(person, personAdmin)
--- lr/glasses/urls.py
+++ lr/glasses/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,LoginView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('', LoginView.as_view(), name='login'),
 ]
